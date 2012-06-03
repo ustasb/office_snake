7e1dfbeba7b5b8c0a1aa62d331d671d6ec84fab9
Brian Ustas <bjjustas@gmail.com>
2012-06-03 16:55:28 +0200

    hsSetter/Getter file refactoring.

--- cgi-bin/hsGetter.py
+++ cgi-bin/hsGetter.py
@@ -5,15 +5,23 @@ import cgi
 request = cgi.FieldStorage()
 scoresToLoad = int(request.getvalue('amt'))
 difficulty = request.getvalue('diff')
+hsFile = 'cgi-bin/{0}HS.csv'.format(difficulty)
 
 # Print header
 print('Content-type: text/plain\r')
 print('\r')
 
-hsFile = open('cgi-bin/' + difficulty + 'HS.csv')
-for line in hsFile:
-    print(line, end='')
+try:
+    hsFile = open(hsFile, 'r')
+except IOError:
+    # Create the high score file.
+    open (hsFile, 'w')
+    hsFile = open(hsFile, 'r')
 
-    scoresToLoad -= 1
-    if scoresToLoad == 0:
-        break
+with hsFile:
+    for line in hsFile:
+        print(line, end='')
+
+        scoresToLoad -= 1
+        if scoresToLoad == 0:
+            break
--- cgi-bin/hsSetter.py
+++ cgi-bin/hsSetter.py
@@ -11,44 +11,54 @@ pDifficulty = request.getvalue('diff')
 pScore = int(request.getvalue('score'))
 pTime = int(request.getvalue('time'))
 
+hsFile = 'cgi-bin/{0}HS.csv'.format(pDifficulty)
+
 date = time.strftime('%m/%d/%y') 
-entry = '{0},{1},{2},{3}\n'.format(pName, pScore, pTime,
+newEntry = '{0},{1},{2},{3}\n'.format(pName, pScore, pTime,
                                    time.strftime('%m/%d/%y'))
 
+# Print header
 print('Content-type: text/plain\r')
 print('\r')
 
+rank = 1
 madeEntry = False
-hsFile = open('cgi-bin/' + pDifficulty + 'HS.csv', 'r+')
-outfile = open('cgi-bin/tmp.txt', 'w+')
 
-for rank, line in enumerate(hsFile):
+try:
+    hsFile = open(hsFile, 'r')
+except IOError:
+    # Create the high score file.
+    open (hsFile, 'w')
+    hsFile = open(hsFile, 'r')
 
-    if madeEntry is False:
-        score = int(re.match(r'\w*,(\d*),', line).group(1))
-        if pScore > score:
-            outfile.write(entry)
-            outfile.write(line)
-            print(rank)
-            madeEntry = True
-        elif pScore == score:
-            print(rank)
-            madeEntry = True
-            time = int(re.match(r'\w*,\d*,(\d*),', line).group(1))
-            if pTime < time:
-                outfile.write(entry)
+with hsFile, open('cgi-bin/tmp.txt', 'w') as outfile:
+    for rank, line in enumerate(hsFile):
+
+        if madeEntry is False:
+            score = int(re.match(r'\w*,(\d*),', line).group(1))
+            if pScore > score:
+                outfile.write(newEntry)
                 outfile.write(line)
+                print(rank)
+                madeEntry = True
+            elif pScore == score:
+                print(rank)
+                madeEntry = True
+                time = int(re.match(r'\w*,\d*,(\d*),', line).group(1))
+                if pTime < time:
+                    outfile.write(newEntry)
+                    outfile.write(line)
+                else:
+                    outfile.write(line)
+                    outfile.write(newEntry)
             else:
                 outfile.write(line)
-                outfile.write(entry)
         else:
             outfile.write(line)
-    else:
-        outfile.write(line)
 
-if madeEntry is False:
-    print(rank)
-    outfile.write(entry)
+    if madeEntry is False:
+        print(rank)
+        outfile.write(newEntry)
 
 os.rename('cgi-bin/tmp.txt', 'cgi-bin/' + pDifficulty + 'HS.csv')
 
