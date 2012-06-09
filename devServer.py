0fd7ad40a35b9d8675dfb68bbf29cdc1b2d45fa2
Brian Ustas <bjjustas@gmail.com>
2012-06-10 00:05:12 +0200

    Now accepting argument specifying which folder to serve.

--- devServer.py
+++ devServer.py
@@ -1,8 +1,14 @@
 #!/usr/local/bin/python3
 
 import os
+import sys
 import http.server
 
-# Move into the public/ directory to begin serving it.
-os.chdir('public')
+# Move into the supplied directory to begin serving it.
+try: 
+    os.chdir(sys.argv[1])
+except IndexError:
+    # Default to the public/ directory.
+    os.chdir('public')
+
 http.server.HTTPServer(("", 8000), http.server.CGIHTTPRequestHandler).serve_forever()
