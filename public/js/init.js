91d47b5649c0a0356beedd9c5ebdc41e6ef4ef40
Brian Ustas <brianustas@gmail.com>
2013-03-10 23:39:49 -0400

    Add Rakefile to compress + minify JS

--- public/js/init.js
+++ public/js/init.js
@@ -1,46 +1,239 @@
-View.initialize(800, 540);
+$(function () {
+    View.initialize(800, 540);
 
-// Build and prepare the custom attribute sliders.
-$(".slider").each(function () {
-    var min, max, defaultValue, type = $(this).prev().attr("id");
-    
-    switch (type) {
-    case "speed":
-        min = 1;
-        max = 20;
-        defaultValue = 10;
-        break;
-    case "powerUpDuration":
-        min = 1;
-        max = 30;
-        defaultValue = 5;
-        break;
-    case "startingLength":
-        min = 1;
-        max = 100;
-        defaultValue = 6;
-        break;        
-    case "humansPresent":
-        max = 50;
-        defaultValue = 20;
-        break;
-    case "walls":
-        max = Helpers.getMaxWalls();
-        defaultValue = Math.floor(max * 0.5);
-        break;
-    case "bombs":
-        defaultValue = 50;
-        break;
-    }
-    
-    $("#" + type).children("span").text(defaultValue);
-    
-    $(this).slider({
-        min: min || 0,
-        max: max || 100,
-        value: defaultValue || 1,
-        slide: function (event, ui) {
-            $(this).prev().children("span").text(ui.value);
+    // Build and prepare the custom attribute sliders.
+    $(".slider").each(function () {
+        var min, max, defaultValue, type = $(this).prev().attr("id");
+
+        switch (type) {
+        case "speed":
+            min = 1;
+            max = 20;
+            defaultValue = 10;
+            break;
+        case "powerUpDuration":
+            min = 1;
+            max = 30;
+            defaultValue = 5;
+            break;
+        case "startingLength":
+            min = 1;
+            max = 100;
+            defaultValue = 6;
+            break;
+        case "humansPresent":
+            max = 50;
+            defaultValue = 20;
+            break;
+        case "walls":
+            max = Helpers.getMaxWalls();
+            defaultValue = Math.floor(max * 0.5);
+            break;
+        case "bombs":
+            defaultValue = 50;
+            break;
         }
-    }).attr("id", type + "Slider");
+
+        $("#" + type).children("span").text(defaultValue);
+
+        $(this).slider({
+            min: min || 0,
+            max: max || 100,
+            value: defaultValue || 1,
+            slide: function (event, ui) {
+                $(this).prev().children("span").text(ui.value);
+            }
+        }).attr("id", type + "Slider");
+    });
+
+    // Bind events
+    $(document.body).keydown(function (event) {
+        var key = event.keyCode;
+
+        // Prevents the player from immediately reversing directions
+        // via cycling directions.
+        if (Engine.waitingForInput && Snake.head) {
+            switch (key) {
+            case 87: // w
+            case 38: // Up Arrow
+                if (Snake.head.direction !== "s") {
+                    Snake.head.direction = "n";
+                    Engine.waitingForInput = false;
+                }
+                break;
+            case 68: // d
+            case 39: // Right Arrow
+                if (Snake.head.direction !== "w") {
+                    Snake.head.direction = "e";
+                    Engine.waitingForInput = false;
+                }
+                break;
+            case 83: // s
+            case 40: // Down Arrow
+                if (Snake.head.direction !== "n") {
+                    Snake.head.direction = "s";
+                    Engine.waitingForInput = false;
+                }
+                break;
+            case 65: // a
+            case 37: // Left Arrow
+                if (Snake.head.direction !== "e") {
+                    Snake.head.direction = "w";
+                    Engine.waitingForInput = false;
+                }
+                break;
+            }
+        }
+
+        // Pause if 'Esc', 'Space' or 'p' is pressed.
+        if (key === 27 || key === 32 || key === 80) {
+            Engine.pause();
+        }
+    });
+
+    $(window).resize(function () {
+        View.updateGameContainer(View.gameContWidth, View.gameContHeight);
+    });
+
+    $(".back").click(function () {
+        $(".ui-resizable-handle").hide();
+
+        var loadingEl = document.getElementById("loading");
+        if (loadingEl) {
+            $(loadingEl).remove();
+        }
+
+        View.resizeMainWindow("homePos", View.initWidth, View.initHeight);
+        Cache.session.difficulty = undefined;
+    });
+
+    $("#mainMenu span").click(function () {
+        var action = $(this).text().toLowerCase();
+
+        if (action === "high scores") {
+            View.resizeMainWindow("highScoresPos", false, false, function () {
+                View.loadHighScores(View.highScoresView);
+            });
+        } else {
+            View.selectDifficulty(action);
+        }
+
+        if (action === "high scores" || action === "custom") {
+            $(".ui-resizable-handle").show();
+        }
+    });
+
+    $("#highScoresView span").click(function () {
+        var difficulty = $(this).text().toLowerCase();
+        if (difficulty !== " | ") {
+            View.highScoresView = difficulty;
+            View.loadHighScores(View.highScoresView);
+        }
+    });
+
+    $("#ready").click(function () {
+        if (Cache.session.difficulty === "custom") {
+            $(".ui-resizable-handle").hide();
+        }
+
+        View.initSession();
+
+        View.resizeMainWindow("mapsPos", false, false, function () {
+            Engine.countdown(3);
+        });
+    });
+
+    $("#retry").click(Helpers.retry);
+
+    $("#resume").click(Engine.resume);
+
+    $(".goToMenu").click(function () {
+        View.resizeMainWindow("homePos", View.initWidth, View.initHeight, function () {
+            $("#gameOver").appendTo("#gameViewUtils").hide();
+            Helpers.clearLevel();
+            View.removeLevel(Cache.session.level);
+            View.resetSession();
+        });
+    });
+
+    $("#gameContainer").resizable({
+        handles: "se",
+        minWidth: 500,
+        minHeight: 500,
+        resize: function (event, ui) {
+            View.updateGameContainer(ui.size.width, ui.size.height);
+            View.updateViewDependencies(ui.size.width, ui.size.height);
+        },
+        stop: function (event, ui) {
+            View.alignGameWinToGrid(Cache.literals.tileWidth,
+                                    Cache.literals.tileHeight);
+
+            Helpers.readjustWallSlider();
+
+            // Deal with the number of high scores on display.
+            if (View.slidingMenuTab === "highScoresPos") {
+                if (ui.originalSize.height < ui.size.height) {
+                    View.loadHighScores(View.highScoresView);
+                } else {
+                    var numOfScores, scoreRows, scoresToDelete;
+
+                    numOfScores = $(".highscore").length;
+                    scoreRows = (ui.size.height - 4 * Cache.literals.tileHeight) /
+                                Cache.literals.tileHeight;
+                    scoresToDelete = numOfScores - scoreRows;
+
+                    for (var i = 0; i < scoresToDelete; i++) {
+                        $(".highscore").last().remove();
+                    }
+                }
+            }
+
+        }
+    });
+
+    $("#submit").live("click", function () {
+        var name, nameLength, handleSuccess, minLen = 3, maxLen = 15,
+            $submit = $("#enterName #submit");
+
+        name = $("input[name='name']").val();
+        nameLength = name.length;
+
+        $submit.hide();
+
+        // Validate the given name.
+        if (name.search(/\W/) === -1 && nameLength >= minLen &&
+            nameLength <= maxLen) {
+            var $saving = $("#enterName .saving");
+            $saving.show().text("Saving...");
+
+            handleSuccess = function(rank) {
+                $("#rank").text("Rank: " + rank);
+                $("#enterName input").attr("disabled", "disabled");
+                $saving.text("Saved.");
+            };
+
+            eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('32 10=["\\22\\19\\15\\12","\\33\\31\\28\\30","\\17\\17\\29\\18\\34\\11\\39\\35\\11\\15\\29\\21\\13\\28\\12\\14\\14\\12\\25\\17\\38\\24","\\22\\11\\26\\26\\11\\18\\37\\23\\14\\24","\\13\\12\\13\\13\\11\\19\\15","\\14\\11\\36\\12","\\13\\18\\19\\25\\12","\\23\\11\\15\\22\\14","\\21\\20\\13\\21","\\20\\40\\20\\50"];$[10[9]]({51:10[1],49:10[2],47:{27:27,42:16[10[4]][10[3]],48:43[10[5]],44:16[10[4]][10[6]],46:16[10[4]][10[6]]+45[10[8]](16[10[4]][10[6]]+10[7])}})[10[0]](41);',10,52,'||||||||||_0x17a8|x69|x65|x73|x74|x6E|Cache|x2E|x63|x6F|x61|x68|x64|x6C|x79|x72|x66|name|x53|x2F|x54|x4F|var|x50|x67|x62|x6D|x75|x70|x2D|x6A|handleSuccess|diff|Engine|score|Sha1|hash|data|time|url|x78|type'.split('|'),0,{}));
+
+        } else {
+            var $error, errorMsg;
+
+            if (nameLength < minLen) {
+                errorMsg = "Names must be at least " + minLen + " characters.";
+            } else if (nameLength > maxLen) {
+                errorMsg = "Names must be less than " + (maxLen + 1) +
+                           " characters.";
+            } else {
+                errorMsg = "Names can only contain alphanumeric characters.";
+            }
+
+            $error = $("#enterName .error");
+            $error.show().text(errorMsg);
+
+            setTimeout(function () {
+                $error.fadeOut(1500, function () {
+                    $submit.show();
+                });
+            }, 1000);
+        }
+    });
 });
--- public/js/view.js
+++ public/js/view.js
@@ -1,7 +1,7 @@
 var View = {
     initWidth: 0,
     initHeight: 0,
-    // Record what the gameContainer's dimensions should be because the 
+    // Record what the gameContainer's dimensions should be because the
     // browser can't be expected to return accurate measurements when zooming.
     gameContWidth: 0,
     gameContHeight: 0,
@@ -20,13 +20,13 @@ var View = {
             navigator.platform.indexOf("iPod") !== -1) {
             $("#clientWarning").text("Sorry, this site requires a keyboard.");
         } else if ($.browser && $.browser.msie && $.browser.version <= 6.0) {
-            $("#clientWarning").html("Your browser isn't supported" + 
+            $("#clientWarning").html("Your browser isn't supported" +
                                      "here.<br />Update it!");
         } else {
             $("#clientWarning").remove();
             $("#pauseMenu, #gameOver, .ui-resizable-handle").hide();
             $("#gameContainer, #fbLike, #credit").show();
-            
+
             // Sanitize arguments
             width = (width > 500) ? width : 500;
             width -= (width % Cache.literals.tileWidth);
@@ -35,20 +35,20 @@ var View = {
 
             View.initWidth = width;
             View.initHeight = height;
-            
+
             View.updateGameContainer(width, height);
             View.updateViewDependencies(width, height);
-            
+
             var maxWidth = $(window).width() / 1.3,
-                maxHeight = $(window).height() / 1.3;            
-            
+                maxHeight = $(window).height() / 1.3;
+
             maxWidth -= (maxWidth % Cache.literals.tileWidth);
             maxHeight -= (maxHeight % Cache.literals.tileHeight);
-            
+
             maxWidth = (maxWidth > width) ? maxWidth : width + 100;
             maxHeight = (maxHeight > height) ? maxHeight : height + 100;
             maxWidth = (maxWidth < 1500) ? maxWidth : 1500;
-            maxHeight = (maxHeight < 1100) ? maxHeight : 1100;                
+            maxHeight = (maxHeight < 1100) ? maxHeight : 1100;
 
             $("#gameContainer").resizable("option", {
                 maxWidth: maxWidth,
@@ -58,24 +58,24 @@ var View = {
     },
     loadHighScores: function (difficulty) {
         var currentTime = (new Date()).getTime();
-        
+
         // Prevents the user from overloading the server with requests.
         if (currentTime - View.hsRequestTime > 250) {
             var scorePadding, scoresToLoad, handleSuccess;
-            
+
             View.hsRequestTime = (new Date()).getTime();
-            
+
             scorePadding = 4 * Cache.literals.tileHeight;
             scoresToLoad = Math.floor((View.gameContHeight - scorePadding) /
                                       Cache.literals.tileHeight);
-            
+
             $("#" + difficulty + "View").css("text-decoration", "underline")
             .siblings().css("text-decoration", "none");
-            
+
             if (!document.getElementById("loading")) {
                 $("#canvas").append('<div id="loading">Loading...</div>');
             }
-            
+
             handleSuccess = function(csvResponse) {
                 var $csvRow, time, rank = 1;
 
@@ -85,24 +85,24 @@ var View = {
                     // trailing '\n'.
                     csvResponse.pop();
                 }
-                
+
                 $(".highscore, #loading").remove();
-                
+
                 for (var i = 0, j = csvResponse.length; i < j; i++) {
                     csvResponse[i] = csvResponse[i].split(",");
                     time = View.formatTimeStr(csvResponse[i][2]);
                     if (time.length < 3) {
                         time += "s";
                     }
-                    
+
                     $csvRow = $("<tr class='highscore'><td>" + rank +
                                 "</td><td>" + csvResponse[i][0] + "</td>" +
                                 "<td>" + csvResponse[i][1] + "</td><td>" +
-                                time + "</td><td>" + csvResponse[i][3] + 
+                                time + "</td><td>" + csvResponse[i][3] +
                                 "</td></tr>");
 
                     $("#highScores table").append($csvRow);
-                    
+
                     rank += 1;
                 }
             };
@@ -120,7 +120,7 @@ var View = {
     // Sets the 'left' positions of the sliding menu tabs.
     alignMenuTabs: function (width) {
         var posLeft, posIncrement = 0;
-        
+
         for (posLeft in View.slidingMenu) {
             if (View.slidingMenu.hasOwnProperty(posLeft)) {
                 View.slidingMenu[posLeft] = posIncrement;
@@ -130,7 +130,7 @@ var View = {
     },
     formatTimeStr: function (seconds) {
         var minutes;
-        
+
         if (seconds / 60 >= 1) {
             minutes = Math.floor(seconds / 60);
             seconds -= (minutes * 60);
@@ -140,21 +140,21 @@ var View = {
             return minutes + ":" + seconds;
         } else {
             return seconds;
-        }    
+        }
     },
     alignGameWinToGrid: function (tileWidth, tileHeight) {
         var validWidth = View.gameContWidth,
             validHeight = View.gameContHeight;
-        
+
         validWidth -= View.gameContWidth % tileWidth;
         validHeight -= View.gameContHeight % tileHeight;
-        
+
         View.updateGameContainer(validWidth, validHeight);
         View.updateViewDependencies(validWidth, validHeight);
     },
     centerElement: function ($element, $parent) {
         var topPad, attr;
-        
+
         $parent = $parent || $("#gameContainer");
         attr = ($element.css("position") === "static") ? "marginTop" : "top";
         topPad = ($parent.height() - $element.outerHeight()) / 2;
@@ -165,16 +165,16 @@ var View = {
     },
     updateChallengeInfo: function (level, gems) {
         var progress = "Hidden Humans: " + gems;
-        
+
         if (Cache.session.difficulty === "challenge") {
             progress = "Level: " + level + " | " + progress;
         }
-        
+
         $("#snakeHUD .challengeInfo").text(progress);
     },
     updateGameContainer: function (width, height) {
         var top = ($(document).height() - height) / 2;
-                
+
         $("#gameContainer").css("top", top + "px")
         .width(width)
         .height(height);
@@ -186,22 +186,22 @@ var View = {
         var $slidingMenu = $("#slidingMenu"),
             $centerBoxes = $(".centerBox"),
             $heightDepend = $("#canvas, #slidingMenu"),
-            $viewDepend = $("#canvas, #highScores, #home, #help, #maps " + 
+            $viewDepend = $("#canvas, #highScores, #home, #help, #maps " +
                             ".levelContainer, #gameViewUtils");
 
         return function (width, height) {
             if (width) {
                 $viewDepend.width(width);
-                
+
                 View.alignMenuTabs(width);
                 $slidingMenu.css("left",
                                  View.slidingMenu[View.slidingMenuTab] + "px");
             }
-            
+
             if (height) {
                 $heightDepend.height(height);
             }
-            
+
             $centerBoxes.each(function () {
                 View.centerElement($(this));
             });
@@ -211,11 +211,11 @@ var View = {
     selectDifficulty: function (difficulty) {
         Cache.session.difficulty = difficulty;
         $("#selectedDifficulty").text("difficulty: " + difficulty);
-        
+
         if (difficulty === "custom") {
             $(".challengeInfo, #instructions, #pickUpCtrlInfo").hide();
             $("#custom, #snakeHUD .challengeInfo").show();
-            
+
             Helpers.readjustWallSlider();
             View.centerElement($("#help .centerBox"));
             View.resizeMainWindow("helpPos", false, false);
@@ -228,18 +228,18 @@ var View = {
             } else {
                 $(".challengeInfo").show();
             }
-            
+
             View.resizeMainWindow("helpPos", Cache.literals.compWidth,
                                   Cache.literals.compHeight);
         }
-        
+
     },
     resizeMainWindow: (function () {
 
         var $gameContainer = $("#gameContainer"),
             animateMenu = function (menuTab, callback) {
             View.slidingMenuTab = menuTab;
-            
+
             $("#slidingMenu").animate({
                 "left" : View.slidingMenu[menuTab] + "px"
             }, {
@@ -248,7 +248,7 @@ var View = {
                 complete: callback
             });
         };
-        
+
         return function (menu, width, height, callback) {
             width = width ? width : View.gameContWidth;
             height = height ? height : View.gameContHeight;
@@ -270,7 +270,7 @@ var View = {
                     }
                 }
             });
-            
+
             View.alignMenuTabs(width);
             animateMenu(menu, callback);
         };
@@ -279,19 +279,19 @@ var View = {
     updateScore: function (points) {
         points = (Engine.activeDblPoints) ? points * 2 : points;
         Cache.session.score += points;
-        
+
         $("#scoreBar").text(Cache.session.score + "pts");
 
         points = (points > 0) ? "+" + points : points;
         $("#pointAddition").html("<span>" + points + "pts<span>");
-        
+
         if (Cache.addedPtsTimer) {
             clearTimeout(Cache.addedPtsTimer);
         }
-        
+
         Cache.addedPtsTimer = setTimeout(function () {
             if ($.browser && $.browser.msie && $.browser.version < 9.0) {
-                // This is an IE < 9 fix. Firstly, fadeout() is too processor 
+                // This is an IE < 9 fix. Firstly, fadeout() is too processor
                 // intensive for IE and slows the game down noticeably.
                 // It is, however, a desirable effect for faster browsers.
                 // Finally, removing the span isn't immediate--the span hangs
@@ -310,23 +310,23 @@ var View = {
         .append('<div id="level_' + Cache.session.level +
                 '" class="level"><div id="countdown"><div>3</div></div></div>')
         .appendTo("#mapsIE7Fix");
-        
+
         if (Cache.session.difficulty === "challenge") {
             $("#countdown").prepend("<div id='currentLevel'>Level " +
                                     Cache.session.level + "</div>");
         } else {
             View.centerElement($("#countdown div"), $("#countdown"));
         }
-        
+
         View.centerElement($("#countdown"));
     },
     powerUpTimeBar: function (time) {
         time *= 1000; // Convert to milliseconds.
-    
+
         if (!document.getElementById("powerUpTimeBar")) {
             $("<div id='powerUpTimeBar'></div>").appendTo("#powerUpPlaceHolder");
         }
-        
+
         $("#powerUpTimeBar").animate({
             width: "0px"
         }, time, function () {
@@ -338,7 +338,7 @@ var View = {
             Snake.segsToCreate = Snake.initSegs;
             Engine.powerUpDuration = 5;
         }
-        
+
         switch (Cache.session.difficulty) {
         case "classic":
             Snake.speed = 76;
@@ -347,7 +347,7 @@ var View = {
         case "challenge":
             Snake.speed = 120;
             Cache.session.humansPresent = 0;
-            
+
             // Determine the final level based on the level size.
             var maxWalls = Helpers.getMaxWalls();
             Cache.session.finalLevel = maxWalls / Cache.literals.wallMultiplier;
@@ -362,16 +362,16 @@ var View = {
                           $("#speedSlider").slider("value")) * 10 + 20;
             break;
         }
-        
+
         Helpers.buildTiles();
         View.buildLevel();
         Helpers.prepareLevel(Snake.initSegs, Cache.session.humansPresent);
     },
     resetSession: function () {
         Cache.resetCache();
-        
+
         Engine.powerUpToggleTime = 0;
-        
+
         $("#pauseMenu").hide();
         $("#clock").text(Engine.time = 0);
         $("#scoreBar").text(Cache.session.score + "pts");
@@ -380,10 +380,10 @@ var View = {
         if (document.getElementById("powerUpTimeBar")) {
             $("#powerUpTimeBar").stop();
         }
-        
+
         $("#score").text("Score: " + Cache.session.score);
         $("#rank").text("Rank: -");
-        
+
         if (Cache.session.difficulty === "custom") {
             $("#rank, #enterName").hide();
         } else {
@@ -395,7 +395,7 @@ var View = {
                 $("#rank, #enterName").hide();
             }
         }
-        
+
         $("#gameOver").appendTo("#level_" +
                                 Cache.session.level).width(View.gameContWidth);
         $("#gameOver").show();
