ef77699124f44f2009d5a8c7f82792f378f69274
Brian Ustas <brianustas@gmail.com>
2019-02-18 13:08:47 -0500

    Update NPM dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,10 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        mangle: true
+        compress: true,
+        mangle: {
+          toplevel: true,
+        },
       },
       dist: {
         files: {
